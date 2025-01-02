50b68591af1b078e3f0b01bccd5c3fd4a076946a
Kirill <pesterevkv2003@gmail.com>
2025-01-02 11:35:03 +0300

    fix errors

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import AboutMovieView from '../views/AboutMovieView.vue'
-import MovieListView from '../views/MovieListView.vue'
+import AboutMovieView from '@/views/AboutMovieView.vue'
+import MovieListView from '@/views/MovieListView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
